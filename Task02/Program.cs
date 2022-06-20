7ece9e4f3b9e34e21a7d27a5490fc08d3f688905
BlackbirdXX <superblackbirdzz@gmail.com>
2022-06-20 11:06:37 +0300

    Задача 02

---
+++ Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Программа, которая принимает на вход число и выдаёт сумму цифр в числе.");
+WriteLine("Введите число:");
+int n = Convert.ToInt32(ReadLine());
+
+WriteLine($"-> {NumberSum(n)}");
+
+int NumberSum(int x)
+{
+    int result = 0;
+    for (int i = x; i > 0; i /= 10)
+    {
+        int intermediate = i % 10;
+        result = result + intermediate;
+    }
+    return result;
+}
